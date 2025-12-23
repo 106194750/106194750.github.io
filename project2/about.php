2d8f56f0a9dc595071eb95d6299c0f18b265f502
hana <106194750@student.swin.edu.au>
2025-12-23 14:00:00 +0300

    nav turned into php include

---
+++ project2/about.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+<meta charset="UTF-8">
+<title>About Us</title>
+<link rel="stylesheet" href="styles/styles.css">
+
+</head>
+<body>
+<?php include 'nav.inc'; ?>
+<article>
+    <h1>About Us</h1> <figure>
+        <img src="images/pic.jpeg" alt="HAZ Group Photo">
+        <figcaption>Our Team - HAZ Web Designs</figcaption>
+    </figure>
+    <section id="id">
+            <h2>Our Group HAZ Skillz</h2>
+<ol >
+    <li>Hana Ouaida</li>
+    <ul><li>ID 106194750</li></ul>
+    <li>Asma Hossain</li>
+    <ul><li>ID 106196099</li></ul>
+    <li>Zahra Hossain</li>
+    <ul><li>ID 106232278</li></ul>
+</ol>
+    </section>
+    
+<section>
+    <h2>Contributions</h2>
+    <dl>
+    <dt>Hana Ouaida</dt>    
+    <dd>Wrote the HTML for apply.html, about.html, and the menu, and wrote the general CSS.</dd>
+    <dt>Asma Hossain</dt>
+    <dd>Wrote the HTML for index.html, created and styled the footer, created the logo.</dd>
+    <dt>Zahra Ahmad</dt>
+    <dd>Wrote the HTML for jobs.html and styled the form in apply.html using CSS.</dd>
+</dl>
+
+</section>
+
+<section class="tab"><table>
+    
+    <caption>Our Interests</caption>
+    <tr>
+        <th>Name</th>
+        <th>Country of origin</th>
+        <th>Favourite song</th>
+        <th>Favourite book</th>
+        <th colspan="3">Interests</th>
+    </tr>
+    <tr>
+        <td>Hana Ouaida</td>
+        <td>Lebanon</td>
+        <td>No Release by Micheal Bowman</td>
+        <td>Never Let Me Go by Kazuo Ishiguro</td>
+        <td>Scifi</td>
+        <td>Crochet</td>
+        <td>Linux</td>
+    </tr>
+    <tr>
+        <td>Asma Hossain</td>
+        <td>Bangladesh</td>
+        <td>Banana by the Minions</td>
+        <td>1Q48 Haruki Murakami</td>
+        <td>Reading</td>
+        <td>Table tennis</td>
+        <td>Formula 1</td>
+    </tr>
+    <tr>
+        <td>Zahra Ahmad</td>
+        <td>Pakistan</td>
+        <td>Sweater Weather by The Neighborhood </td>
+        <td>Cinder Marissa Meyer</td>
+        <td>Astronomy</td>
+        <td>Writing</td>
+        <td>Video games</td>
+    </tr>
+</table></section>
+
+</article>
+</body>
+
+</html>
