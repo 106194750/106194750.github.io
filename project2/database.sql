20edcaea5c0f0e2a33d880681aa8cdba433a22e9
hana <106194750@student.swin.edu.au>
2026-01-06 10:13:41 +0300

    process_eoi.php now sends data from apply.php to the database

--- project2/database.sql
+++ project2/database.sql
@@ -1,8 +1,11 @@
+USE haz
+
 CREATE TABLE IF NOT EXISTS eoi (
     EOInumber INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     jobref VARCHAR(20) NOT NULL,
     fname VARCHAR(20) NOT NULL,
     lname VARCHAR(20) NOT NULL,
+    dob VARCHAR(10) NOT NULL,
     phone INT(8) NOT NULL,
     email VARCHAR(40) NOT NULL,
     skill1 VARCHAR(30) NOT NULL,
@@ -11,7 +14,7 @@ CREATE TABLE IF NOT EXISTS eoi (
     skill4 VARCHAR(30) NOT NULL,
     skill5 VARCHAR(30) NOT NULL,
     otherSkills VARCHAR(500)
-    status ENUM('New', 'Current', 'Final')
+    eoiStatus ENUM('New', 'Current', 'Final')
 );
 
 CREATE TABLE IF NOT EXISTS address (
@@ -20,6 +23,5 @@ CREATE TABLE IF NOT EXISTS address (
     suburb VARCHAR(40) NOT NULL,
     addressZone INT(2) NOT NULL,
     city ENUM('Doha', 'Al Wakra', 'Al Khor', 'Dukhan', 'Al Shamal', 'Mesaieed', 'Ras Laffan') NOT NULL,
-    postcode INT(10) NOT NULL, 
-    FOREIGN KEY (EOInumber)  REFERENCES eoi(EOInumber),
+    FOREIGN KEY (EOInumber)  REFERENCES eoi(EOInumber)
 );
